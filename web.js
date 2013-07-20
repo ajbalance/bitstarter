9453c00cdae4d561ccaaf687e7ae2a8479409825
ajbalance <ajb.7777777@gmail.com>
2013-07-20 07:07:27 +0000

    Debugging web.js

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var fs = require('fs');
-var outFile = 'index.html';
-var buffer = new Buffer(fs.readFileSync(outfile);
-var outTextStr = buf.toString(buffer);
+//var fs = require('fs');
+//var outFile = 'index.html';
+//var buffer = new Buffer(fs.readFileSync(outfile);
+//var outTextStr = buffer.toString('utf-8');
 
 app.get('/', function(request, response) {
   response.send('restesting');
